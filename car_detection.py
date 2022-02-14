c1901026a3e157c633094bfc6fb292206950511f
NNICEEt <niceteerapat@gmail.com>
2022-02-15 00:12:27 +0700

    final

--- car_detection.py
+++ car_detection.py
@@ -1,1 +1,86 @@
-print("car detection");
+import cv2
+import numpy as np
+from tracker_main import *
+
+# Create tracker object
+tracker = EuclideanDistTracker()
+
+cap = cv2.VideoCapture("1.mp4")
+H = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+W = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+# Object detection from Stable camera
+object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=100)
+
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (45, 45))
+
+id = 0
+
+iframe = 0
+
+total = 0
+s = 0
+m = 0
+l = 0
+
+roi_position = {'x': (700, 1500), 'y': (250, 800)}  # (x1, x2), (y1, y2)
+
+while True:
+    _, frame = cap.read()
+
+    if not _:
+        break
+
+    iframe += 1
+
+    # Extract Region of interest
+    x1_roi, x2_roi = roi_position['x']
+    y1_roi, y2_roi = roi_position['y']
+    roi = frame[y1_roi:y2_roi, x1_roi:x2_roi]
+
+    # 1. Object Detection
+    mask = object_detector.apply(roi)
+    _, mask = cv2.threshold(mask, 50, 255, cv2.THRESH_BINARY)
+    mask = cv2.erode(mask, np.ones((3, 3)))
+    closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    contours, _ = cv2.findContours(closing, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    detections = []
+    for cnt in contours:
+        # Calculate area and remove small elements
+        area = cv2.contourArea(cnt)
+        if area > 2000:
+            x, y, w, h = cv2.boundingRect(cnt)
+            detections.append([x, y, w, h])
+
+    cv2.line(frame, (0, 525 + int(H - y2_roi)), (1600, 525 + int(H - y2_roi)), (0, 255, 0), 2)
+    cv2.line(frame, (0, 320 + int(H - y2_roi)), (1600, 320 + int(H - y2_roi)), (0, 255, 0), 2)
+
+    # 2. Object Tracking
+    object_tracking = tracker.update(detections, iframe, fps)
+    for i, box_id in enumerate(object_tracking['objects_bbs_ids']):
+        x, y, w, h, id, _ = box_id
+        speed = object_tracking['speeds'][id]
+        cv2.putText(roi, f'No. {id}', (x, y - 15), cv2.FONT_HERSHEY_PLAIN, 1.5, (255, 0, 0), 2)
+        cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        if speed is not None:
+            cv2.putText(roi, f'{speed} km/h', (x + 110, y - 15), cv2.FONT_HERSHEY_PLAIN, 1.5, (255, 0, 0), 2)
+
+    total = object_tracking['total']
+    s = object_tracking['s_total']
+    m = object_tracking['m_total']
+    l = object_tracking['l_total']
+
+    cv2.putText(frame, f'total : {total}', (900, 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+    cv2.putText(frame, f'small : {s}', (1100, 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+    cv2.putText(frame, f'medium : {m}', (1100, 100), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+    cv2.putText(frame, f'large : {l}', (1100, 150), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+    # cv2.imshow("roi", roi)
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Mask", closing)
+    cv2.waitKey(1)
+
+print(f'total: {total}')
+print(f'small: {s}')
+print(f'medium: {m}')
+print(f'large: {l}')
---
+++ tracker_main.py
@@ -0,0 +1,95 @@
+import math
+import time
+
+
+class EuclideanDistTracker:
+    def __init__(self):
+        # Store the center positions of the objects
+        self.center_points = {}
+        # Store the positions and size of the objects
+        self.vehicle_points = {}
+        # Store frame_usage of the objects
+        self.vehicle_frame_usage = {}
+        # Store the speed of the objects
+        self.vehicle_speeds = {}
+        # Keep the count of the IDs
+        # each time a new object id detected, the count will increase by one
+        self.id_count = 0
+        self.s_id_count = 0
+        self.m_id_count = 0
+        self.l_id_count = 0
+
+    def update(self, objects_rect, iframe, fps):
+        # Objects boxes and ids
+        objects_bbs_ids = []
+
+        # Get center point of new object
+        for rect in objects_rect:
+            x, y, w, h = rect
+            cx = (x + x + w) // 2
+            cy = (y + y + h) // 2
+
+            # Find out if that object was detected already
+            same_object_detected = False
+            for id, pt in self.vehicle_points.items():
+                x_pt, y_pt, w_pt, h_pt, is_count = pt
+                cx_pt = (x_pt + x_pt + w_pt) // 2
+                cy_pt = (y_pt + y_pt + h_pt) // 2
+
+                iframe_pt, _ = self.vehicle_frame_usage[id]
+
+                dist = math.hypot(cx - cx_pt, cy - cy_pt)
+
+                # validate new car
+                t = 5
+                if dist < 390 and x_pt - t < cx < x_pt + w_pt + t:
+
+                    if (w_pt * h_pt) * 0.5 > w * h:
+                        x, y, w, h = x_pt, y_pt, w_pt, h_pt
+
+                    if cy < 350 and is_count == False:
+                        is_count = True
+                        if w <= 150:
+                            self.s_id_count += 1
+                        elif w <= 310:
+                            self.m_id_count += 1
+                        else:
+                            self.l_id_count += 1
+
+                        t = (iframe - iframe_pt) / fps
+                        print(t)
+                        v = (4.5 / t) * 3.6 # 4.4m
+                        self.vehicle_speeds[id] = "{:.2f}".format(v)
+                        iframe_pt = iframe
+
+                    self.vehicle_points[id] = (x, y, w, h, is_count)
+                    self.vehicle_frame_usage[id] = (iframe_pt, iframe)
+                    objects_bbs_ids.append([x, y, w, h, id, is_count])
+                    same_object_detected = True
+                    break
+
+            # New object is detected we assign the ID to that object
+            if same_object_detected is False and cy > 400:
+                self.id_count += 1
+                self.vehicle_points[self.id_count] = (x, y, w, h, False)
+                self.vehicle_frame_usage[self.id_count] = (iframe, None)
+                self.vehicle_speeds[self.id_count] = None
+                objects_bbs_ids.append([x, y, w, h, self.id_count, False])
+
+        # Clean the dictionary by center points to remove IDS not used anymore
+        new_vehicle_points = {}
+        for obj_bb_id in objects_bbs_ids:
+            _, _, _, _, object_id, _ = obj_bb_id
+            vehicle_point = self.vehicle_points[object_id]
+            new_vehicle_points[object_id] = vehicle_point
+
+        # Update dictionary with IDs not used removed
+        self.vehicle_points = new_vehicle_points.copy()
+        return {
+            'objects_bbs_ids': objects_bbs_ids,
+            'total': self.id_count,
+            's_total': self.s_id_count,
+            'm_total': self.m_id_count,
+            'l_total': self.l_id_count,
+            'speeds': self.vehicle_speeds
+        }
